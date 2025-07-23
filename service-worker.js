50780b958701cf162f530cd01f50d28069707f9f
Manakoste <spkostenko07@gmail.com>
2025-07-23 11:51:49 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'transport-company-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/vehicles.html',
+  '/routes.html',
+  '/style.css',
+  '/js/vehicles.js',
+  '/js/routes.js',
+  '/data/vehicles.json',
+  '/data/routes.json',
+  '/images/hero-bg.jpg',
+  '/images/volvo-truck.jpg',
+  '/images/man-truck.jpg',
+  '/images/sprinter.jpg',
+  '/images/trailer.jpg',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
